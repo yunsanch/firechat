9d29a3555438912e34d268a42de791b409c781dd
yunsanch <60577975+yunsanch@users.noreply.github.com>
2024-03-30 11:53:42 -0400

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Yunior Sanchez on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
